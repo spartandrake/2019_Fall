948807c9e420ce197d77c6c830de9c7803ae7ca7
Samantha Burkey <sburkey@wvup.edu>
2019-09-28 21:12:33 -0400

    Finished todo application homework!

--- sburkey/Samantha_Burkey_React_Todo/samantha_burkey_todo/src/App.js
+++ sburkey/Samantha_Burkey_React_Todo/samantha_burkey_todo/src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoItems from './TodoItems';
+
+const Header = () => {
+  return(
+    <div>
+      <h1>Samantha Burkey Todo App</h1>
+    </div>
+  )
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+
+      <Todo />
+
     </div>
   );
 }
 
-export default App;
+class Todo extends React.Component{
+  constructor(props) {
+    super(props);
+   
+    this.state = {
+      items: []
+    };
+   
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+  
+  delete(key) {
+    this.props.delete(key);
+  }
+
+  addItem(e) {
+    if (this._inputElement.value !== "") {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+   
+      this.setState((prevState) => {
+        return { 
+          items: prevState.items.concat(newItem) 
+        };
+      });
+     
+      this._inputElement.value = "";
+    }
+     
+    console.log(this.state.items);
+       
+    e.preventDefault();
+  }
+
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App-header">
+        <Header />
+        <div className="todoList">
+          <div className="header">
+            <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a} 
+                    placeholder="Task...">
+              </input>
+              <button type="submit">Add Task</button>
+            </form>
+          </div>
+          <TodoItems entries={this.state.items}
+                 deleteItem={this.deleteItem}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Todo;
--- sburkey/Samantha_Burkey_React_Todo/samantha_burkey_todo/src/index.js
+++ sburkey/Samantha_Burkey_React_Todo/samantha_burkey_todo/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Todo from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var destination = document.querySelector(".container");
+  
+ReactDOM.render(
+    <div>
+        <Todo />
+    </div>,
+    
+    destination
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
