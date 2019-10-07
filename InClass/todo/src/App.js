d1acc21c8bad8e104c355a842da2ca034f6ad150
Aaron Freeland <afreeland@gmail.com>
2019-10-07 18:43:22 -0400

    Updated todo with comments & more editing features

--- InClass/todo/src/App.js
+++ InClass/todo/src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, Container, Input, List } from 'semantic-ui-react';
+import { Button, Container, Input, List, Radio } from 'semantic-ui-react';
 import moment from 'moment';
 
 
 function TodoList() {
+  const [state, setState] = useState({
+    // The id of the todo that the user is trying to edit
+    currentEditableTodoId: null,
+    // This is a storage for the actual input value when it changes
+    // It will be used to update the todo label once they save their changes
+    editValue: null,
+  });
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -23,21 +30,104 @@ function TodoList() {
     }
   ]);
 
-  console.log(todos);
+  /**
+   * 
+   * @param {string} todoId The id of the todo
+   * 
+   * Map our todos and find the todo that has been toggled to completed/not completed
+   */
+  const handleToggle = (todoId) => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    })
+    // Now that we have our data lets update our state store
+    setTodos(newTodos);
+  }
+
+  /**
+   * Here we want to actually take the data the user entered and update
+   * the todo that they were trying to edit =)
+   */
+  const handleEdit = () => {
+    const updatedTodos = todos.map(todo => {
+      // If we find the todo, lets put in some work
+      if (todo.id === state.currentEditableTodoId) {
+        // Use the value of the input that they have been typing
+        todo.label = state.editValue;
+
+        // Reset the state so the next edit is off to a clean start
+        setState({
+          currentEditableTodoId: null,
+          editValue: null
+        });
+      }
+      return todo;
+    })
+
+    // Update our todo state store
+    setTodos(updatedTodos);
+  }
   return (
     <List divided relaxed>
       {
         todos.sort((a, b) => a.completed - b.completed).map(todo => {
+          // Lets take the date and generate a relative time that is friendly
           const relativeTime = moment(todo.dateCreated).fromNow()
           const labelStyle = {};
+          // We would like a specific style to use when a todo is completed for the ux
           if (todo.completed) {
             labelStyle.textDecoration = 'line-through';
           }
+
+          // Since we now keep track of the todo the user wants to edit, we can handle this 
+          // specifically and render out a custom input option inline
+          if (todo.id === state.currentEditableTodoId) {
+            return (
+              <List.Item>
+                <Radio toggle style={{ verticalAlign: 'unset' }} disabled checked={todo.completed} />
+                <List.Content style={{
+                  display: 'inline-block',
+                  paddingLeft: '20px'
+                }}>
+
+                  <List.Header as='a' style={labelStyle}>
+                    <Input value={state.editValue} onChange={(event, { value }) => {
+                      // Every time the user types we need to update our `editValue` so that
+                      // we have a safe way to edit a todo in an immutable fashion
+                      setState({
+                        // Spread the current state so we do not lose any values
+                        ...state,
+                        // Update our temporary label holder while the user is working
+                        editValue: value
+                      })
+                    }} />
+                    <Button primary onClick={handleEdit}>Save</Button>
+                  </List.Header>
+                </List.Content>
+              </List.Item>
+            )
+          }
+
           return (
             <List.Item>
-              <List.Icon name='github' size='large' verticalAlign='middle' />
-              <List.Content>
-                <List.Header as='a' style={labelStyle}>{todo.label}</List.Header>
+              <Radio toggle style={{ verticalAlign: 'unset' }} onClick={() => {
+                handleToggle(todo.id);
+              }} checked={todo.completed} />
+              <List.Content style={{
+                display: 'inline-block',
+                paddingLeft: '20px'
+              }}>
+                <List.Header as='a' style={labelStyle} onClick={() => {
+                  if (todo.completed) return;
+                  setState({
+                    currentEditableTodoId: todo.id,
+                    editValue: todo.label
+                  })
+                }}>{todo.label}</List.Header>
+
                 {!todo.completed && <List.Description as='a'>{relativeTime}</List.Description>}
               </List.Content>
             </List.Item>
