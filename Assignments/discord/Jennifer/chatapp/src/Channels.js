1086c7fc5e28e080417166442fd98959df7d1943
Aaron Freeland <afreeland@gmail.com>
2019-10-14 19:03:03 -0400

    More in depth walk through of props

--- Assignments/discord/Jennifer/chatapp/src/Channels.js
+++ Assignments/discord/Jennifer/chatapp/src/Channels.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 
 
-
-class Channels extends Component {
-  
- 
-
-    render() {
+const Channels = (props) => {
   return (
-  
     <div>
-    <h4># General</h4>
-    <h4># Resources</h4>
-  </div>
-    
+      <h4># General</h4>
+      <h4># Resources</h4>
+    </div>
   );
 }
-}
+
+
+// class Channels extends Component {
+
+
+
+//   render() {
+//     return (
+
+//       <div>
+//         <h4># General</h4>
+//         <h4># Resources</h4>
+//       </div>
+
+//     );
+//   }
+// }
 
 export default Channels;
