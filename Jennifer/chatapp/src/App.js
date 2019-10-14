5044566b7ab828d7f5cc38bccd87648ab434fe4c
Jennifer Mackey <jennym1211@hotmail.com>
2019-10-14 06:13:41 -0400

    idk what i'm doing
    but i'm trying

--- Jennifer/chatapp/src/App.js
+++ Jennifer/chatapp/src/App.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Channels from './Channels';
 import Messages from './Messages';
 import ChatInput from './ChatInput';
 import Chatters from './Chatters';
+import { Button, Container, Input, List, Radio } from 'semantic-ui-react';
+import moment from 'moment';
 
-function App() {
+
+class App extends React.Component
+{
+  
+  
+
+
+  render() {
   return (
     <div className="App">
+      
+    
+
+
+
+<div class="ui vertically divided grid">
+  <div class="three column row">
+    <div class="column">
     <div id="ChannelsContainer"><Channels /></div>
-    <div id="MessagesContainer"><Messages /></div>
-    <div id="ChatInputContainer"><ChatInput /></div>
+    </div>
+    <div class="column">
+    <div id="MessagesContainer"><Messages/></div>
+    </div>
+   
+    <div class="column">
     <div id="Chatters"><Chatters /></div>
     </div>
+    </div>
+    </div>
+</div>
   );
 }
+}
+
 
 export default App;
