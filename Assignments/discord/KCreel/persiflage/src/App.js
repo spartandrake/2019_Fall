6fbb235320957abd060d52c5d2fbc3f939c26a72
Kristopher Creel <31522425+spartandrake@users.noreply.github.com>
2019-10-17 17:45:31 -0400

    restarting discord

--- Assignments/discord/KCreel/persiflage/src/App.js
+++ Assignments/discord/KCreel/persiflage/src/App.js
@@ -1,15 +1,17 @@
 import React, {useState} from 'react';
 import './App.css';
-import Server from './Server'
-const uuidv1 = require('uuid/v1');
 
+const uuidv1 = require('uuid/v1');
+//[items, setItem] = useState(
 function App() {
-  const [servers, setServers] = useState([{serverImage:'/image1.png', users:['Test1', 'Test2',... 'Test10'], serverName:'Server1', key:uuidv1()}, 
-                                          {serverImage:'/image2.png', users:['Test11', 'Test12',... 'Test20'], serverName:'Server2', key:uuidv1()}])
+  const [currentUser, setCurrentUser] = useState({username: ''});
+  const [currentServer, setCurrentServer] = useState({servername: ''});
+  const userList;
+  const serverList;
   return (
     <div className="App">
       <div className='Server'>
-        <Server serverItems={servers}/>  
+        
       </div>
     </div>
   );
